2e509075cc9d6dd6ae92c2996cc4839e2d148b66
Arnas <arnasdrazdys15@gmail.com>
2023-03-18 23:02:08 +0200

    Files upload

---
+++ eParduotuve/Data/NetDBContext.cs
@@ -0,0 +1,16 @@
+﻿using eParduotuve.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace eParduotuve.Data
+{
+	public class NetDBContext : IdentityDbContext
+	{
+		public NetDBContext(DbContextOptions options) : base(options)
+		{
+		}
+
+		public DbSet<Admin> shopAdmin { get; set; }
+		public DbSet<AppUser> appUser { get; set; }
+	}
+}
